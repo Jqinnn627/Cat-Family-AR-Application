0b4831ca0e21fbb5094a76329a4801e2b0a81a0b
Jqinnn627 <jiaqin627@gmail.com>
2025-09-20 13:43:49 +0800

    Change main menu button language

--- Assets/Script/LanguageManager.cs
+++ Assets/Script/LanguageManager.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LanguageManager : MonoBehaviour
 {
+    public Sprite enViewCatImgBtn;
+    public Sprite cnViewCatImgBtn;
+    public Sprite malayViewCatImgBtn;
+    public Sprite enQuizImgBtn;
+    public Sprite cnQuizImgBtn;
+    public Sprite malayQuizImgBtn;
+    public Sprite enQuitImgBtn;
+    public Sprite cnQuitImgBtn;
+    public Sprite malayQuitImgBtn;
+
+    [Header("UI References")]
     public GameObject languagePanel;
+    public Button viewCatImgUI;
+    public Button quizImgUI;
+    public Button quitImgUI;
+     
    public void EnLang()
     {
         LanguageScript.SetLanguage("en");
         languagePanel.SetActive(false);
+        viewCatImgUI.image.sprite = enViewCatImgBtn;
+        quizImgUI.image.sprite = enQuizImgBtn;
+        quitImgUI.image.sprite = enQuitImgBtn;
     }
     public void CnLang()
     {
         LanguageScript.SetLanguage("cn");
         languagePanel.SetActive(false);
+        viewCatImgUI.image.sprite = cnViewCatImgBtn;
+        quizImgUI.image.sprite = cnQuizImgBtn;
+        quitImgUI.image.sprite = cnQuitImgBtn;
     }
     public void MelayuLang()
     {
         LanguageScript.SetLanguage("malay");
         languagePanel.SetActive(false);
+        viewCatImgUI.image.sprite = malayViewCatImgBtn;
+        viewCatImgUI.image.sprite = malayQuizImgBtn;
+        quitImgUI.image.sprite = malayQuitImgBtn;
     }
     public void chooseLang()
     {
